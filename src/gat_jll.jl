ba3979fd745ef9cae1fc040756a7b95da6473b7c
SatoshiTerasaki <terasakisatoshi.math@gmail.com>
2024-11-29 18:18:41 +0900

    gat_jll build 0.19.1+0

--- src/gat_jll.jl
+++ src/gat_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("gat")
-JLLWrappers.@generate_main_file("gat", UUID("b88765aa-e0a2-5b02-b897-840ea4e6307e"))
+JLLWrappers.@generate_main_file("gat", Base.UUID("b88765aa-e0a2-5b02-b897-840ea4e6307e"))
 end  # module gat_jll
